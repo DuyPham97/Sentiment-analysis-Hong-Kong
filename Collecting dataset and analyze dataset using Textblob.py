a1834cf5236f5cf3a48fe45e2747d1e5a67cc20b
DuyPham97 <josh.pham@minerva.kgi.edu>
2019-11-26 17:40:42 +0700

    Add files via upload

---
+++ Collecting dataset and analyze dataset using Textblob.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Add your Twitter API credentials
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+auth = tw.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tw.API(auth, wait_on_rate_limit=True)
+
+#I will search all the words with dataset
+search_term = "hong+kong -filter:retweets"
+tweets = tw.Cursor(api.search,
+                   q=search_term,
+                   lang="en",
+                   since='2019-11-17').items(18000)
+
+#Function to remove the URL from the tweet
+def remove_url(txt):
+    return " ".join(re.sub("([^0-9A-Za-z \t])|(\w+:\/\/\S+)", "", txt).split())
+
+# Remove URLs
+tweets_no_urls = [remove_url(tweet.text) for tweet in tweets]
+
+# Create textblob objects of the tweets ()
+sentiment_objects = [TextBlob(tweet) for tweet in tweets_no_urls]
+
+# Create list of polarity valuesx and tweet text
+sentiment_values = [[tweet.sentiment.polarity, str(tweet)] for tweet in sentiment_objects]
+
+# Create dataframe containing the polarity value and tweet text
+sentiment_df = pd.DataFrame(sentiment_values, columns=["polarity", "tweet"])
+
+# Download stopwords
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+
+# Remove stop words from each tweet list of words
+tweets_nsw = [[word for word in tweet_words if not word in stop_words]
+              for tweet_words in words_in_tweet]
+
+fig, ax = plt.subplots(figsize=(8, 6))
+
+# Plot histogram with break at zero
+sentiment_df.hist(bins=[-1, -0.75, -0.5, -0.25, 0.0, 0.25, 0.5, 0.75, 1],
+             ax=ax,
+             color="purple")
+
+plt.title("Sentiments from Tweets on Hong Kong")
+plt.show()
+
